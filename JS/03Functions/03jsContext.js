d08432a166f7ef9d0e518323f9a87d4002578df5
Mohib Ismail <imohib168@gmail.com>
2021-02-16 23:33:20 +0500

    JS

---
+++ JS/03Functions/03jsContext.js
@@ -0,0 +1,15 @@
+// sayHello();
+
+// function sayHello() {
+//     console.log("Hello");
+// }
+
+if (2 === 2) {
+    console.log("It's true");
+}
+
+var myName = "Mohib";
+
+// if (myName === window.myName) {
+//     console.log("True statement");
+// }
